9c8d9d30dd52bfa05666e0f6888dfd15d19b08b0
Arsh khan <57301016+Arshkhanak@users.noreply.github.com>
2022-11-19 02:42:59 +0530

    Add files via upload

---
+++ app/src/main/kotlin/com/rinki/parent/di/PerActivity.kt
@@ -0,0 +1,10 @@
+package com.rinki.parent.di
+
+import javax.inject.Scope
+
+/**
+ * Created by samuel incoom on 19/02/15.
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerActivity
